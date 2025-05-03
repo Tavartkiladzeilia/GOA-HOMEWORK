59f8bc64ae85bc3c7fc1e3f04d2fa18983f27bf2
Tavartkiladzeilia <tavartkiladzeilia0407@gmail.com>
2025-05-03 21:53:53 +0400

    third commit

---
+++ day 2/lesson02.py
@@ -0,0 +1,14 @@
+thl1=25
+thl2=5
+
+print(thl1+thl2)
+print(thl1-thl2)
+print(thl1*thl2)
+print(thl1/thl2)
+
+name="ilia"
+surname="tavartkiladze"
+age=10
+
+
+("gamargoba, chemi saxelia", name, "chemi gvaria", surname,"chemi asakia",age)
