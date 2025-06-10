36cfd2ca5116ebff8b81a9364785422c416ed57e
Tavartkiladzeilia <tavartkiladzeilia0407@gmail.com>
2025-06-10 20:24:20 +0400

    second commit

---
+++ day7/classwork/day7.py
@@ -0,0 +1,22 @@
+num1=35
+num2=20
+sun=num1+num2
+print(sun)
+
+
+price=50
+print(price+30)
+
+
+name=input("enter your name")
+age=input("enter your age")
+
+# debuging არის შეცდომების გასწორება
+
+# კომენტარი არის როცა ხსნი ან წერ რაც უნდა დაწერო
+
+
+name="ilia"
+Name="ilia"
+
+print("ilia"+ "ilia")
---
+++ day7/homework/day7.py
@@ -0,0 +1,41 @@
+# ჩვენ ვადეკლალირებთ name რომ იყოს ილია.
+
+
+name="ilia"
+surname="tavartkiladze"
+
+print(name+surname)
+
+
+points=50
+points=points+14
+points=points+17
+
+print(points)
+
+
+age = 156
+print(age)
+
+
+
+kla1=27
+kla2=12
+sum=kla1+kla2
+sum=kla1-kla2
+
+
+print(kla1*kla2)
+print(kla1/kla2)
+
+
+
+# age-->134
+# print"age
+       
+age=134
+print(age) 
+
+
+# მე შემიძლია ჩვლადის განახლება და ასევე გამოტანა.ამ დავალებებში ყოველთვის საჭიროა იხმარო #--> ანუ კომენტარი და ასევე ცვლადის შეკრება.
+
