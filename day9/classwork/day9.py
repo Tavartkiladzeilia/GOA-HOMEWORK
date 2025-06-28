d79304825f582c89852dc844c3478748d47e91a8
Tavartkiladzeilia <tavartkiladzeilia0407@gmail.com>
2025-06-28 17:39:11 +0400

    second commit

---
+++ day9/classwork/day9.py
@@ -0,0 +1,26 @@
+name="giorgi"
+age="15"
+
+print(name+age)
+
+# ეს არის ერორი იმიტომ რომ არ არის მეორეში integer
+
+
+name="ilia"
+surname="tavartkiladze"
+age="11"
+psevdonim="tav"
+
+print(type "name+surname+age+psevdonim")
+
+
+num1=20
+num2=5
+
+print( type num1/num2)
+
+
+word="cat"
+num=15
+
+print( type( word*num))
