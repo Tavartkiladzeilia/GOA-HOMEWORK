be6497b139f362b5a7f47f4917dec3e7cb5a6884
Tavartkiladzeilia <tavartkiladzeilia0407@gmail.com>
2025-07-10 11:17:37 +0400

    second commit

---
+++ day11/classwork/lesson01.py
@@ -0,0 +1,64 @@
+# boolean არის < და >  რიცხვების ოპერატორი ან true და false
+
+# 1 ნიშნავს true და 0 ნიშნავს false
+
+
+# and ოპერატორი არის მაგალითად true and false ანუ and ს თუ დაწერ გამოვა false
+
+
+# or ოპერატორი არის მაგალითად true or false ანუ or ს თუ მიუწერ პასუხი იქნება true
+
+
+# 1 davaleba
+print(69>45)
+print(54<76)
+print(89>76)
+print(65>55)
+print(76>66)
+print(90>44)
+print(43>34)
+print(32>23)
+print(11>10)
+print(34>12)
+
+
+
+# 7 davaleba
+print(34>23 or True )
+print(45<54 or True)
+print(65>47 or True)
+print(78<100 or True)
+print(87<99 or True)
+print(65<66 or True)
+print(45<54 or True)
+print(76>66 or True)
+print(33<43 or True)
+print(66<76 or True)
+
+
+# davaleba 6
+
+print(54<44 and False)
+print(34>64 and False)
+print(54<45 and False)
+print(50<32 and False)
+print(53<44 and False)
+print(542<45 and False)
+print(5432<40 and False)
+print(54322<44 and False)
+print(54<41 and False)
+print(67<44 and False)
+
+# davaleba 10
+
+temperature=30
+print(temperature<34)
+
+
+
+
+
+
+
+
+# danarceni ver mivxvdi
---
+++ day11/homework/lesson01.py
@@ -0,0 +1,31 @@
+print(56>454 and False)
+print(6767<777 and False)
+print(8888<76 and False)
+print(343<44 and False)
+print(66>343 and False)
+print(666<545 and False)
+print(545>333 and False)
+print(555<77 and False)
+print(6767>47777 and False)
+print(56>67 and False)
+
+# ეს არის პირველი დავალება!
+
+
+print(54<55)
+print(65>55)
+print(34<43)
+print(66>65)
+print(87>78)
+print(55<65)
+print(8787<777)
+print(90<99)
+print(87>78)
+print(66<67)
+
+# ეს არის მეორე დავალება!
+
+
+
+
+
