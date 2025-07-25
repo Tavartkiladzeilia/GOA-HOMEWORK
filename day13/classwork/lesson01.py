64434ea451b8251b4cd54b22cdb72ed817e825b2
Tavartkiladzeilia <tavartkiladzeilia0407@gmail.com>
2025-07-25 14:47:01 +0400

    second commit

---
+++ day13/classwork/lesson01.py
@@ -0,0 +1,34 @@
+for i in range(10):
+    print(i)
+
+    # ეს არის პირველი დავალება.
+
+for i in range(10):
+    print("ilia")
+    #  ეს არის მეორე დავალება.
+
+surname="tavartkiladze" 
+for i in surname:
+    print(i)
+    # ეს არის მესამე დავალება.
+
+for i in range(2,20):
+    print(i)
+# ეს არის მეოთხე დავალება.
+
+    # 6 ver mivxvdi
+
+    for i in range(75, 150):
+        print(i)
+# ეს არის მეშვიდე დავალება.
+
+    #  მერვე ვერ მივხვდი.
+
+num="64563545244"
+for i in num:
+    print(i)
+
+
+
+
+# მეცხრე დავალება
