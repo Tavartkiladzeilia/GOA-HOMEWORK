75846a6c370c1910e447375084630c9105fa3aff
Tavartkiladzeilia <tavartkiladzeilia0407@gmail.com>
2025-08-09 17:56:42 +0400

    second commit

---
+++ day15/classwork/lesson01.py
@@ -0,0 +1,34 @@
+seats = 10
+while seats > 0:
+    print("ilia")
+    seats = seats - 1
+
+    # ეს არის პირველი დავალება
+
+
+# მეორე ვერ გავიგე
+
+
+
+age = 11
+if age < 18:
+    print("discount")
+
+  
+else:
+    print("regular price")
+
+    # ეს არის მეოთხე დავალება
+
+
+
+age = 19
+if age >= 18:
+    print("regular price")
+
+else:
+    print("discount")
+
+    # ეს არის მეხუთე 
+
+       
