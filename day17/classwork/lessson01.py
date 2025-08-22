6a2f0c8fcc75846a0552b873ed762df11d0c02b3
Tavartkiladzeilia <tavartkiladzeilia0407@gmail.com>
2025-08-22 17:00:20 +0400

    second commit

---
+++ day17/classwork/lessson01.py
@@ -0,0 +1,44 @@
+# while loop - არის ციკლი სადაც იმეორებს რომელიმე დაწერილ სიტყვას.
+# for loop - არის ციკლი სადაც გამოიყენება განსაზღვრული რაოდენობის გამეორებას
+
+# ეს არის 1 დავალება.
+
+
+
+for i in range(10, 50,2):
+    print(i)
+
+    # ეს არის 2 დავალება.
+
+
+i = 0
+while i < 10:
+    print(i)
+    i = i + 2
+
+    # ეს არის 3 დაავალება.
+
+
+age = "34667"
+for i in age:
+    print(i)
+
+    # ეს არის 4 დავალება.
+
+
+
+age = int( input("enter your age: "))
+
+if age > 18:
+    print("you became adult")
+    
+elif age == 18:
+    print("you are adult")
+
+
+
+else:
+    print("you are child")
+
+
+# ეს არის მეხუთე დავალება
---
+++ day17/homework/lesson01.py
@@ -0,0 +1,31 @@
+# elif - გვჭირდება იმაში რომ if else შორის დავამატოთ სხვა რაიმე წინადადება.
+# ეს არის 1 დავალება
+
+
+for i in range(20, 100, 1):
+    print(i)
+
+# ეს არის 2 დავალება.
+
+
+password =  int( input("enter your password: "))
+
+if password == 12345:
+    print("password is corrected")
+
+else:
+    print("incorrected password")
+
+# ეს არის 3 დავალება
+
+
+i = 0
+while i < 100:
+    print(i)
+    i = i + 2
+
+
+
+
+
+
