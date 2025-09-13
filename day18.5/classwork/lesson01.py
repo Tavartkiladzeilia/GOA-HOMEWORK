6116e82ea7f6ef0f17852df222dd868e0f2fcba1
Tavartkiladzeilia <tavartkiladzeilia0407@gmail.com>
2025-09-13 18:59:30 +0400

    second commit

---
+++ day18.5/classwork/lesson01.py
@@ -0,0 +1,30 @@
+# 1 დავალება
+
+for i in range(20, 200, 2):
+    print(i)
+
+
+# 2 დავალება
+
+# ვერ გავიგე და ამიხსენით კიდევ
+
+
+# 3 დავალება
+
+age = int( input("enter your age: "))
+
+if age <12:
+    print("you are a kid!")
+
+elif age > 12:
+    print("you are a teen")
+
+elif age <18:
+    print("you are a teen")
+
+else:
+    print("you are adult")
+
+# 4 დავალება
+# კითხვა ვერ გავიგე
+
